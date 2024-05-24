760ba78ae57ff9982be061387d41d8287859c4ca
trpipher <trpipher@gmail.com>
2024-05-24 19:59:17 -0400

    permanent forge

--- sd_forge/main.sh
+++ sd_forge/main.sh
@@ -11,7 +11,7 @@ trap 'error_exit "### ERROR ###"' ERR
 
 echo "### Setting up Stable Diffusion WebUI Forge ###"
 log "Setting up Stable Diffusion WebUI Forge"
-if [[ "$REINSTALL_SD_FORGE" || ! -f "/tmp/sd_forge.prepared" ]]; then
+if [[ "$REINSTALL_SD_FORGE" || ! -f "/storage/sd_forge.prepared" ]]; then
 
     TARGET_REPO_URL="https://github.com/lllyasviel/stable-diffusion-webui-forge" \
     TARGET_REPO_DIR=$REPO_DIR \
@@ -33,33 +33,42 @@ if [[ "$REINSTALL_SD_FORGE" || ! -f "/tmp/sd_forge.prepared" ]]; then
     #Prepare the controlnet model dir
     #mkdir -p $MODEL_DIR/controlnet/
     # cp $LINK_CONTROLNET_TO/*.yaml $MODEL_DIR/controlnet/
-    rm -rf $VENV_DIR/sd_forge-env
+    # rm -rf $VENV_DIR/sd_forge-env
     
     
-    python3.10 -m venv $VENV_DIR/sd_forge-env
+    # python3.10 -m venv $VENV_DIR/sd_forge-env
     
-    source $VENV_DIR/sd_forge-env/bin/activate
+    source /storage/sd_forge-env/bin/activate
 
-    pip install --upgrade pip
-    pip install --upgrade wheel setuptools
+    # pip install --upgrade pip
+    # pip install --upgrade wheel setuptools
     
-    # fix install issue with pycairo, which is needed by sd-webui-controlnet
-    apt-get install -y libcairo2-dev libjpeg-dev libgif-dev
-    pip uninstall -y torch torchvision torchaudio protobuf lxml tensorflow
+    # # fix install issue with pycairo, which is needed by sd-webui-controlnet
+    # apt-get install -y libcairo2-dev libjpeg-dev libgif-dev
+    # pip uninstall -y torch torchvision torchaudio protobuf lxml tensorflow
 
-    export PYTHONPATH="$PYTHONPATH:$REPO_DIR"
-    # must run inside webui dir since env['PYTHONPATH'] = os.path.abspath(".") existing in launch.py
-    cd $REPO_DIR
-    python $current_dir/preinstall.py
-    cd $current_dir
+    # export PYTHONPATH="$PYTHONPATH:$REPO_DIR"
+    # # must run inside webui dir since env['PYTHONPATH'] = os.path.abspath(".") existing in launch.py
+    # cd $REPO_DIR
+    # python $current_dir/preinstall.py
+    # cd $current_dir
 
-    pip install xformers
-    pip uninstall -y torchvision
-    pip install  torchvision
-    touch /tmp/sd_forge.prepared
+    # pip install xformers
+    # pip uninstall -y torchvision
+    # pip install  torchvision
+    # touch /storage/sd_forge.prepared
 else
-    
-    source $VENV_DIR/sd_forge-env/bin/activate
+    symlinks=(
+        "$REPO_DIR/output:$IMAGE_OUTPUTS_DIR/stable-diffusion-forge"
+        "$MODEL_DIR:$WORKING_DIR/models"
+        "$MODEL_DIR/sd:$LINK_MODEL_TO"
+        "$MODEL_DIR/lora:$LINK_LORA_TO"
+        "$MODEL_DIR/vae:$LINK_VAE_TO"
+        "$MODEL_DIR/hypernetwork:$LINK_HYPERNETWORK_TO"
+        "$MODEL_DIR/embedding:$LINK_EMBEDDING_TO"
+    )
+    prepare_link  "${symlinks[@]}"
+    source /storage/sd_forge-env/bin/activate
     
 fi
 log "Finished Preparing Environment for Stable Diffusion WebUI Forge"
